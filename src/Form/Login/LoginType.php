95034922e11966cbc8002fb7178fed9df0fe13ec
Davor Topalic <zero1geek@gmail.com>
2018-11-19 22:29:42 +0100

    LoginType added.

---
+++ src/Form/Login/LoginType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Form\Login;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LoginType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'validation_groups' => ['sign_in_email'],
+        ]);
+    }
+}
